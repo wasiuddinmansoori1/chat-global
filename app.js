ff79e97da54ffe83a2a7396c528f607fefc9d4b9
Wasiuddin Mansoori <121297231+wasiuddinmansoori1@users.noreply.github.com>
2023-08-02 15:42:33 +0530

    changes made by wasi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 
